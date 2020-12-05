285854f5000e27b0c17d237816d92df1f07291a0
Blake Werner <wernerblake@seattleu.edu>
2020-12-04 23:10:55 -0800

    Fixed error with import. should fix git etiquette.

--- webScraper/spiders/quotes_spider.py
+++ webScraper/spiders/quotes_spider.py
@@ -1,4 +1,4 @@
-import scapy
+import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -6,7 +6,7 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'https://quotes.toscrape.com/page/1/',
-            'https://quotes.toscrape.com/page/2/';
+            'https://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
